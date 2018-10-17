767ca6024a88d324ea5423478417713222e7bb04
fannyhasbi <fnyhsbi@gmail.com>
2018-10-17 16:28:56 +0700

    Import net/http

--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import (
   "fmt"
+  "net/http"
 )
 
+func handleWriter(w http.ResponseWriter, r *http.Request){
+  var message = "Welcome to Parking System"
+  w.Write([]byte(message))
+}
+
 func main(){
-  fmt.Println("Parking System")
+  http.HandleFunc("/", handleWriter)
+
+  var address = "localhost:4040"
+  fmt.Printf("Server running on %s", address)
+  err := http.ListenAndServe(address, nil)
+
+  if err != nil {
+    fmt.Println(err.Error())
+  }
 }
